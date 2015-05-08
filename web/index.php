8573b0bf210ee4a887a8711f2dbc368b3a3b06c4
Gary Rogers <gmrwebde@gmail.com>
2015-05-08 13:30:20 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,25 @@
 <?php
 
-phpinfo();
+require_once __DIR__.'/../vendor/autoload.php'; 
+
+$conn = "mongodb://dev:mypassword@localhost:27017/demoapp";
+$m = new \MongoClient($conn);
+$db = $m->selectDB('demoapp');
+
+$app = new Silex\Application(); 
+
+$app->get('/hello/{name}', function($name) use($app) { 
+
+    $collection = new \MongoCollection($db, 'users');
+    $userQuery = array('email' => 'foo@example.com');
+    $cursor = $collection->find($userQuery);
+    
+    foreach ($cursor as $doc) {
+         echo "<pre>";
+         var_dump($doc);
+    }
+  
+    return 'Hello '.$app->escape($name); 
+}); 
+
+$app->run(); 
